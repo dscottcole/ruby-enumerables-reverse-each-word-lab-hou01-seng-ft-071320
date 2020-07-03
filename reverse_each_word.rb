82f719e3a6e072c50e7c45e32e5a2a7e9bc4486b
Daniel Cole <danielscole1@gmail.com>
2020-07-03 00:02:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   yrra = []
   arry = sentence.split(" ")
   arry.each do {|i| yrra = i.reverse}  
-  binding.pry
   end
+  binding.pry
   yrra_string = yrra.join(" ")
 end
