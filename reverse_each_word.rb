b368447c4ffda217c951a7b13e0f89b65532c629
Daniel Cole <danielscole1@gmail.com>
2020-07-03 00:04:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   yrra = []
   arry = sentence.split(" ")
-  arry.each do {|i| yrra = i.reverse }  
+  arry.each {|i| yrra = i.reverse }  
   yrra_string = yrra.join(" ")
   binding.pry
 end
